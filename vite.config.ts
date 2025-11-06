6ff85ce004b3da8e85b4818afd3c95acbece4080
Denise Xavier dos Santos <denise.santos@firstbps.com.br>
2025-11-06 09:48:15 -0300

    atualizacao 06_04

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,15 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: '/denisexaviereu/',
+export default defineConfig(({ command }) => ({
+
+   
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
+  base: command === 'build' ? '/denisexaviereu/' : '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
